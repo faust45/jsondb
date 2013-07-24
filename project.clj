7d79e6d5321dcedd85b84fef7d785ced73ea83d6
faust45 <faust451@gmail.com>
2013-07-24 17:43:52 +0300

    add demon plugin

--- project.clj
+++ project.clj
@@ -4,11 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [ 
+                  [org.yaml/snakeyaml "1.9"]
                   [org.apache.lucene/lucene-analyzers-common "4.2.0"]
                   [org.apache.lucene/lucene-core "4.2.0"]
                   [org.apache.lucene/lucene-queries "4.2.0"]
                   [cheshire "5.2.0"]
                   [ring "1.2.0"]
                   [compojure "1.1.5"]
-                  [org.clojure/clojure "1.5.1"]])
+                  [org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-daemon "0.5.4"]]
+
+  :daemon {:name-of-service {:ns pmenu.name.space
+           :pidfile "../shared/pmenu.pid"}})
 
